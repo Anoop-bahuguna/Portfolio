8ecd029411a11a112c194e0ef8048cab4904b4f0
Anoop <anup56.ab@gmail.com>
2026-09-21 22:06:52 +0530

    Initial commit

---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,44 @@
+import { getImageUrl } from "../../utils";
+import styles from "./ProjectCard.module.css";
+
+const ProjectCard = ({
+  project: { title, imageSrc, description, skills, demo, status },
+}) => {
+  return (
+    <div className={styles.container}>
+      <img
+        className={styles.image}
+        src={getImageUrl(imageSrc)}
+        alt={`image of ${title}`}
+      />
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
+        {skills.map((skill, id) => {
+          return (
+            <li key={id} className={styles.skill}>
+              {skill}
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.links}>
+        {status && (
+          <span className={styles.notice} role="status">
+            {status}
+          </span>
+        )}
+        <a
+          href={demo}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          Demo
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
